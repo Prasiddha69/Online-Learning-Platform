d3c3f743e81730d1119fb784968671d597d41db2
binayuchai <binayuchai58@gmail.com>
2023-07-22 22:52:33 +0545

    model progressed

--- ols_app/models.py
+++ ols_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampModel(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+
+
