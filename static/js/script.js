84e034a23bb9a38869e471ba00269674230c70b9
binayuchai <binayuchai58@gmail.com>
2023-09-21 00:01:57 +0545

    Navbar responsive

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-       $('.navbar').toggleClass('nav-toggle');
-    });
-});
+// $(document).ready(function(){
+//     $('#menu').click(function(){
+//         $(this).toggleClass('fa-times');
+//        $('.navbar').toggleClass('nav-toggle');
+//     });
+// });
+
+const menu = document.getElementById('menu-btn')
+const navbar = document.getElementById('navbar')
+
+menu.addEventListener('click',()=>{
+    console.log('clicked')
+    navbar.classList.toggle('show')
+})
